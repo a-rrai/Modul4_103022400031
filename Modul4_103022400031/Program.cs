37d9ceff4afd606cf46508330d286e07bb106214
Raissha Najwa Maharani <raissha.njw@gmail.com>
2026-03-25 07:11:25 +0700

    second commit

--- Modul4_103022400031/Program.cs
+++ Modul4_103022400031/Program.cs
@@ -1,4 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System Collections Generic;
 
+namespace Modul4_103022400031
+{
+    public class KodePaket
+    {
+        private static Dictionary<string, string> tabelKodePaket = new Dictionary<string, string>()
+        {
+            {"Basic", "P201"},
+            {"Standard", "P202"},
+            {"Premium", "P203"},
+            {"Unlimited", "P204"},
+            {"Gaming", "P205"},
+            {"Streaming ", "P206"},
+            {"Family", "P207" },
+            {"Business", "P208"},
+            {"Student", "P209"},
+            {"Traveler", "P210"}
+        };
+
+        public static string getKodePaket(string namaPaket)
+        {
+            if (tabelKodePaket.ContainsKey(namaPaket))
+            {
+                return tabelKodePaket[namaPaket];
+            }
+            return "Tidak ada paket";
+        }
+    }
+}
 
